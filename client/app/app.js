295fbf2df030addc47130abf3f3df1f773467835
= <Jmitchell1991@gmail.com>
2016-08-23 18:12:11 -0700

    added loading waiter

--- client/app/app.js
+++ client/app/app.js
@@ -16,7 +16,7 @@ angular.module('app', [])
 		})
 		.catch(function(error) {
 			console.error('we have an error in searchfactory get', error)
-		}) //may need to add error callback here
+		}) 
 	}
 
 	return {
@@ -27,6 +27,7 @@ angular.module('app', [])
 
 .controller('SearchController', function($scope,_, SearchFactory) {		
 	$scope.getSearch = function() {
+		$scope.loading= true
 		SearchFactory.getSearch()
 		.then(function(data) {
 			console.log('scope data is', data.slice(0,100))
@@ -37,6 +38,7 @@ angular.module('app', [])
 			$scope.cats = $scope.dataArr.map(function(dataObj) {
 				return dataObj.category
 			})
+			$scope.loading = false
 		})
 	}
 
@@ -74,73 +76,17 @@ angular.module('app', [])
 	 	})
 	 }
 
-	 //form validation for business, address, category, foodname
-
-
-	// $scope.checkForData = function() {
-	//  	if (!$scope.dataArr) {
-	//  		console.log("there is no dataArr")
-	//  		$scope.noData = true
-	//  	}
-	//  	else if ($scope.dataArr) {
-	//  		$scope.dataArr.forEach(function(dataObj) {
-	//  			console.log('we are in checkfordata')
-	//  			console.log('filterText is', $scope.filterText)
-	//  			console.log('scopeDataObj', dataObj)
-	//  			if (dataObj.business !== undefined) {
-	//  				console.log('dataObj.business: ', dataObj.business)
-	//  				if (dataObj.business.indexOf($scope.filterText) === -1) {
-	//  					console.log('business invalid')
-	//  					$scope.noData = true
-	//  				}
-	//  			}
-	//  			else {
-	//  				$scope.noData = false
-	//  			}
-	//  		})
-	//  	}
-	// }
-
 	
 
-	// $scope.log = function() {
-	// 	$scope.props = $scope.dataArr.map(function(dataObj) {
-	// 	return {
-	// 	business: dataObj.business,
-	// 	category: dataObj.category,
-	// 	city: dataObj.location_1_city,
-	// 	state: dataObj.location_1_state,
-	// 	zipcode: dataObj.zipcode,
-	// 	foodname: dataObj.item,
-	// 	}
-	// })
-
 	
-	
+		$scope.checkForData = function() {
+			$scope.noData = false
+			if (!_.contains($scope.cats, $scope.filterText)) {
+				$scope.noData = true
+			}
+		}
 
 
-// $scope.props = $scope.dataArr.map(function(dataObj) {
-// 			return {
-// 				category: dataObj.category,
-// 				city: dataObj.location_1_city,
-// 				state: dataObj.location_1_state,
-// 				zipcode: dataObj.zipcode,
-// 				foodname: dataObj.item,
-// 				}
-// 			}
-// 		})
-
-
-$scope.checkForData = function() {
-	$scope.noData = false
-	if (!_.contains($scope.cats, $scope.filterText)) {
-		$scope.noData = true
-	}
-}
-
-
-//make an array of [business names, address, category, foodname]
-// check form validation against those
 
 	
 
